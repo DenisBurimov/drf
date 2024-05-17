2d7f4532e1c0c334db85d5ee1915bd448beb3850
denys <denysburimov@gmai.com>
2024-05-17 13:05:24 +0300

    feat: celery scheduled task

--- main/libs/celery.py
+++ main/libs/celery.py
@@ -1,5 +1,8 @@
 import os
+from datetime import timedelta
 from celery import Celery
+from main.logger import log
+from main.apps.articles.models import Article
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main.settings")
 
@@ -9,5 +12,16 @@ app.config_from_object("django.conf:settings", namespace="CELERY")
 
 
 @app.task(bind=True)
-def debug_task(self):
-    print(f"Request: {self.request!r}")
+def debug_task():
+    articles = Article.objects.all()
+    log(log.INFO, "Testing celery task")
+    log(log.INFO, f"Total articles: {len(articles)}")
+
+
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    sender.add_periodic_task(
+        timedelta(minutes=1),
+        debug_task.s(),
+        name="debug task"
+    )
